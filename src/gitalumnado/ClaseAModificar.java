c7b782315dc1f3fc6ee3683868b0d569b297eaaf
ChimoB <ChimoB@192.168.56.1>
2018-11-13 15:17:46 +0100

    Especificación del ejercicio de hoy. Solo espero que salga bien

---
+++ src/gitalumnado/ClaseAModificar.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitalumnado;
+
+/**
+ *
+ * Escribir en esta clase todo lo que os plazca
+ */
+public class ClaseAModificar {
+    
+    
+    // definicion de atributos
+    
+    
+    
+    
+    
+    // definicion de constructores
+    
+    
+    
+    
+    
+    
+    // definicion de metodos
+    
+    
+    
+    
+    // otras definiciones
+    
+}
---
+++ src/gitalumnado/GITAlumnado.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitalumnado;
+
+/**
+ *
+ * @author ChimoB
+ */
+public class GITAlumnado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+            Tarea a realizar por cada alumno:
+             crear una nueva rama con vuestro nombre a partir de la rama alumando
+             Crear una clase con vuesta inicial y vuestro apellido 
+             Escribir el codigo siguiente cada uno
+                edwin: crear un metodo que divida un numero por 0
+                diego: crear un metodo que acceda a una posicion inexistente
+                daniel: acceder a una clase que no existe
+                oscar: acceder a un método de una clase que no existe
+                adrian: asignar un valor que no es del tipo de dato que el atributo
+                antonio: usar un atributo que no está declarado
+                alejandro: usar un metodo de un tipo de dato incompatible
+        
+        */
+    }
+    
+}

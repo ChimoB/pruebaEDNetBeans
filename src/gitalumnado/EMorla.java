611e70d45924707a87c10d2fad0a6c26f79cee49
alumnoDAM <alumnoDAM@144-002>
2018-11-13 20:11:39 +0100

    añadido metodo de divicion de un numero por 0

---
+++ src/gitalumnado/EMorla.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitalumnado;
+
+/**
+ *
+ * @author alumnoDAM
+ */
+public class EMorla {
+    public static void main(String[] args) {
+        int num, resultado;
+        Scanner teclado = new Scanner(System.in);
+    System.out.println("dime un numero");
+        num = teclado.nextIn();
+        resultado = num/0;
+        System.out.println(resultado);
+    }
+}

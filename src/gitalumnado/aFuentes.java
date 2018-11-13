bd5144465e8ea3683e65d3b65dbe95a0564ebd76
alumnoDAM <alumnoDAM@144-014>
2018-11-13 19:53:39 +0100

    Añadido metodo de un tipo de dato incompatible

---
+++ src/gitalumnado/aFuentes.java
@@ -0,0 +1,21 @@
+package gitalumnado;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alumnoDAM
+ */
+public class aFuentes {
+    
+     public static void main(String[] args){
+         int suma;
+         
+         suma.indexOf();
+     }
+    
+}
